316ed01a87581058ad227c1e168e50c37e2595f0
Mukul Patel <Mukul215@gmail.com>
2022-01-10 16:13:21 -0500

    added a card that will eventually approve or reject post

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import { avatar } from "@ngneat/falso";
+import ApprovalCard from "./ApprovalCard";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Matt
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 5:42PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <ApprovalCard>
+        <CommentDetail
+          name="Sam"
+          timeAgo="Today at 4:45 PM"
+          blogPost="This is my first post!"
+          avatar={avatar()}
+        />
+      </ApprovalCard>
+      <CommentDetail
+        name="Alex"
+        timeAgo="Today at 5:45 PM"
+        blogPost="How is everyone doing?!"
+        avatar={avatar()}
+      />
+      <CommentDetail
+        name="Bob"
+        timeAgo="Today at 2:45 AM"
+        blogPost="Welcome to the internet!"
+        avatar={avatar()}
+      />
     </div>
   );
 };
